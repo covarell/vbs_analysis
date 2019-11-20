698d565d4e74798e8685f63c023db5d715ec7643
Roberto <roberto.covarelli@cern.ch>
2019-11-20 14:54:06 +0100

    adapt script

--- 4l_channel/runbkg.sh
+++ 4l_channel/runbkg.sh
@@ -1,17 +1,11 @@
 for chan in 2e2mu 4mu 4e
 	do
-	for cate in  1 #0 #2 
+	for year in 2016 2017 2018 
 		do
-		for highmass in 2 
-			do
-		    for year in 2016 2017 2018 
-		           do
 		  	      
-				#bsub -C 0 -q cmscaf1nd job_bkg.lsf "$chan" $cate $highmass 0
-				#bsub -C 0 -q cmscaf1nd job_bkg.lsf "$chan" $cate $highmass 0
-				./job_bkg.lsf "$chan" $cate $highmass 1 $year
+		#bsub -C 0 -q cmscaf1nd job_bkg.lsf "$chan" $cate $highmass 0
+		#bsub -C 0 -q cmscaf1nd job_bkg.lsf "$chan" $cate $highmass 0
+			./job_bkg.lsf "$chan" $year
 			done
 			done
-			done
-                        done
 
